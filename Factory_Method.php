8a1f348501ee819e9ba3e2683ab71ecbc2dd4f8a
Tealty <1131180370@qq.com>
2017-08-29 00:23:22 +0800

    singleton and factory method

---
+++ Factory_Method.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/28 0028
+ * Time: 下午 11:16
+ */
+
+//工厂方法 Factory Method
+//修改是封闭的 扩展是开发的
+interface db{
+    function conn();
+}
+
+interface Factory{
+    function createDB();
+}
+
+class dbmysql implements db{
+    public function conn()
+    {
+        echo 'mysql conn success!!!';
+    }
+}
+
+class mysqlFactory implements Factory{
+    public function createDB()
+    {
+        return new dbmysql();
+    }
+}
+
+class dbsqlite implements db{
+    public function conn()
+    {
+        echo 'dbsqlite conn success!!!';
+    }
+}
+
+class sqliteFactory implements Factory{
+    public function createDB()
+    {
+        return new dbsqlite();
+    }
+}
+
+
+//==========服务器添加oracle类====
+//前面的代码不用改
+class dboracle implements db{ //写接口
+    public function conn()
+    {
+        echo 'oracle conn success!!!';
+    }
+}
+
+class oracleFactory implements Factory{ //创建接口的连接
+    public function createDB(){
+        return new dboracle();
+    }
+}
+
+
+//==========客户端开始============
+$fact = new mysqlFactory();
+$db   = $fact->createDB();
+$db->conn();
+
+$fact = new sqliteFactory();
+$db   = $fact->createDB();
+$db->conn();
+
+$fact = new oracleFactory();
+$db   = $fact->createDB();
+$db->conn();
---
+++ singleton.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/28 0028
+ * Time: 下午 11:42
+ */
+
+//单例模式 singleton
+/*class sigle{
+
+}
+
+$s1 = new sigle();
+$s2 = new sigle();
+
+//注意，2个对象是1个的时候，才全等
+if( $s1 === $s2 ){
+
+}*/
+
+class sigle{
+    protected static $ins = null;
+
+    public static function getIns()
+    {
+        if( self::$ins===null ){
+            self::$ins = new sigle();
+        }
+
+        return self::$ins;
+    }
+
+    //方法前家final，则方法不能被覆盖，类前家final，则类不能被继承
+    final protected function __construct()
+    {
+
+    }
+
+    //封锁clone
+    final protected function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+
+}
+
+$s1 = sigle::getIns();
+$s2 = sigle::getIns();
+
+if( $s1 === $s2 ) {
+    echo 'one';
+}else{
+    echo 'two';
+}
